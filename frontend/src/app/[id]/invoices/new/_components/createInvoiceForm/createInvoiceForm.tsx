5fae0dd28e8890bbf4572cff151b7701aab34fb4
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-11 22:46:33 +0900

    front_add_create-invoice_yup #42

--- frontend/src/app/[id]/invoices/new/_components/createInvoiceForm/createInvoiceForm.tsx
+++ frontend/src/app/[id]/invoices/new/_components/createInvoiceForm/createInvoiceForm.tsx
@@ -5,10 +5,40 @@ import FinancialSummary from '@/components/financialSummary/financialSummary'
 import MainFood from '@/components/mainFood/mainFood'
 import Notes from '@/components/notes/notes'
 import ProductsTable from '@/components/productsTable/organisms/productsTable/productsTable'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 export default function CreateInvoiceForm() {
-  const { register, handleSubmit, control } = useForm()
+  const errorScheme = yup.object().shape({
+    documentIssueDate: yup.date().required(),
+    documentNumber: yup.string().required(),
+    customerName: yup.string().required(),
+    customerTitle: yup.string(),
+    businessDetails: yup.string().required(),
+    mSealsId: yup.string(),
+    notes: yup.string(),
+    invoiceProducts: yup.array().of(
+      yup.object().shape({
+        itemOrder: yup.number().required(),
+        transactionDate: yup.date(),
+        productName: yup.string(),
+        quantity: yup.number(),
+        unit: yup.string(),
+        price: yup.number(),
+        taxClassification: yup.string().required(),
+      })
+    ),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(errorScheme),
+  })
 
   const onSubmit = (data: any) => {
     const invoiceProducts = data.invoiceProducts.map(
@@ -18,8 +48,29 @@ export default function CreateInvoiceForm() {
     console.log(newData)
   }
 
+  // Yupのエラーのメッセージを配列に格納する
+  const errorMessages = Object.values(errors)
+    .filter((error) => error.message !== undefined)
+    .map((error) => error.message && error.message)
+
+  // const invoiceProductsError0 = errors.invoiceProducts?.[0]
+  // const invoiceProductsError1 = errors.invoiceProducts?.[1]
+  // const invoiceProductsError2 = errors.invoiceProducts?.[2]
+  // const invoiceProductsError3 = errors.invoiceProducts?.[3]
+  // const invoiceProductsError4 = errors.invoiceProducts?.[4]
+
+  // 'undefined' の可能性があるオブジェクトを呼び出すことはできません。ts(2722)
+  // invoiceProductsのエラーが表示できないため、余裕が出来次第対応する。
+  // const invoiceProductsErrors = errors.invoiceProducts
+  // if (invoiceProductsErrors !== undefined) {
+  //   invoiceProductsErrors.map(() => {})
+  // }
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {errorMessages.map((errorMassage) => (
+        <p key={errorMassage}>{errorMassage}</p>
+      ))}
       <DocumentDetails register={register} />
       <ProductsTable register={register} control={control} />
       <FinancialSummary control={control} />
