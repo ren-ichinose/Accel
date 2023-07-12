c426ee2c45ddc7f005e535e74e1947bc44312080
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-12 21:56:53 +0900

    front_add_pages_auth #44

--- frontend/src/app/(auth)/users/register/layout.tsx
+++ frontend/src/app/(auth)/users/register/layout.tsx
@@ -3,9 +3,10 @@ import 'src/styles/globals.css'
 
 export const metadata = {
   title: '新規登録',
-  description: 'ユーザー登録ページです。',
+  description: '新規登録ページです。',
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  // eslint-disable-next-line
+  return <>{children}</>
 }
