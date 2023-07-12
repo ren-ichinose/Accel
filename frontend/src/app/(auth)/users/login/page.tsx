ebce6347170201bfbafdaf5ca067fc9832ed1fdf
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-12 22:04:31 +0900

    front_add_pages_login #44

--- frontend/src/app/(auth)/users/login/page.tsx
+++ frontend/src/app/(auth)/users/login/page.tsx
@@ -1,3 +1,28 @@
+import Button from '@/components/common/atoms/button/button'
+import InputWithLabel from '@/components/common/molecules/inputWithLabel/inputWithLabel'
+import AuthFood from '../../_components/auth/atoms/formFood/authFood'
+import AuthHead from '../../_components/auth/molecules/authHead/authHead'
+
 export default function Login() {
-  return <div>ログイン</div>
+  return (
+    <>
+      <AuthHead title="ログイン" />
+      <InputWithLabel
+        title="ログインID"
+        placeholder="ログインIDを入力してください"
+        inputId="loginId"
+        height="48px"
+        marginBottom="32px"
+      />
+      <InputWithLabel
+        title="パスワード"
+        placeholder="パスワードを入力してください"
+        inputId="password"
+        height="48px"
+        marginBottom="32px"
+      />
+      <Button className="authSubmid" type="submit" text="ログイン" />
+      <AuthFood href="/users/register" text="新規登録はこちら" />
+    </>
+  )
 }
