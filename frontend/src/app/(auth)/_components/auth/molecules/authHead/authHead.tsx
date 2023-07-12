5589f0294c58cb49909ff57be55fcd07029f500f
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-12 23:26:57 +0900

    front_fix_components_auth_style #44

--- frontend/src/app/(auth)/_components/auth/molecules/authHead/authHead.tsx
+++ frontend/src/app/(auth)/_components/auth/molecules/authHead/authHead.tsx
@@ -1,11 +1,21 @@
 import { TbShieldCheck } from 'react-icons/tb'
 import styles from './authHead.module.css'
 
-export default function AuthHead({ title }: { title: string }) {
+export default function AuthHead({
+  title,
+  className = 'title',
+}: {
+  title: string
+  className?: string
+}) {
   return (
     <>
       <TbShieldCheck className={styles.icon} />
-      <h2 className={styles.title}>{title}</h2>
+      <h2 className={styles[className]}>{title}</h2>
     </>
   )
+}
+
+AuthHead.defaultProps = {
+  className: 'title',
 }
