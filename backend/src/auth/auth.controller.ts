004d6ea3424b8c93603b4ec6bcaa8a1a3fedf2e0
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-13 10:03:53 +0900

    back_fix_cors #44

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -31,7 +31,7 @@ export class AuthController {
     const { accessToken } = await this.authService.login(dto);
     res.cookie('access_token', accessToken, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
       path: '/',
     });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   await app.listen(3005);
