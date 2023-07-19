7eeb353eab769daee0ee4a85d6ed20a8adc22b59
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-19 09:12:45 +0900

    front_fix_responsive #62

--- frontend/src/components/header/atoms/logo/logo.tsx
+++ frontend/src/components/header/atoms/logo/logo.tsx
@@ -1,12 +1,5 @@
-import Link from 'next/link'
 import styles from './logo.module.css'
 
-export default function Logo({ busunessId }: { busunessId: string }) {
-  return (
-    <h1>
-      <Link href={`/${busunessId}`} className={styles.logoText}>
-        MyApp
-      </Link>
-    </h1>
-  )
+export default function Logo() {
+  return <h1 className={styles.logoText}>MyApp</h1>
 }
