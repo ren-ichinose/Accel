b6a6b053b0a8582005ea10d871c27c81b84d9f21
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-19 10:56:45 +0900

    front_fix_header_add-icon #62

--- frontend/src/components/header/atoms/headerNavItem/headerNavItem.tsx
+++ frontend/src/components/header/atoms/headerNavItem/headerNavItem.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import postData from '@/utils/postData'
 import { useQueryClient } from '@tanstack/react-query'
+import { HiLogout, HiPencil } from 'react-icons/hi'
+import { HiWindow } from 'react-icons/hi2'
 import styles from './headerNavItem.module.css'
 
 export default function HeaderNavItem({
@@ -24,10 +26,47 @@ export default function HeaderNavItem({
     router.push('/users/login')
   }
 
+  const createNavItem = (navItemId: string) => {
+    switch (navItemId) {
+      case 'home':
+        return (
+          <Link href={href} className={styles.item}>
+            <span className={styles.icon}>
+              <HiWindow />
+            </span>
+            {title}
+          </Link>
+        )
+      case 'createInvoice':
+        return (
+          <Link href={href} className={styles.item}>
+            <span className={styles.icon}>
+              <HiPencil />
+            </span>
+            {title}
+          </Link>
+        )
+      case 'logout':
+        return (
+          <button className={styles.item} type="button" onClick={handleClick}>
+            <span className={styles.icon}>
+              <HiLogout />
+            </span>
+            {title}
+          </button>
+        )
+      default:
+        return null
+    }
+  }
+
   return (
     <li className={styles.container} key={id}>
-      {id !== 'logout' && (
+      {/* {id !== 'logout' && (
         <Link href={href} className={styles.item}>
+          <span className={styles.icon}>
+            <BiHomeAlt2 />
+          </span>
           {title}
         </Link>
       )}
@@ -35,7 +74,8 @@ export default function HeaderNavItem({
         <button className={styles.item} type="button" onClick={handleClick}>
           {title}
         </button>
-      )}
+      )} */}
+      {createNavItem(id)}
     </li>
   )
 }
--- frontend/src/components/header/atoms/logo/logo.tsx
+++ frontend/src/components/header/atoms/logo/logo.tsx
@@ -1,5 +1,13 @@
+import { HiLightningBolt } from 'react-icons/hi'
 import styles from './logo.module.css'
 
 export default function Logo() {
-  return <h1 className={styles.logoText}>MyApp</h1>
+  return (
+    <h1 className={styles.logoText}>
+      <span className={styles.icon}>
+        <HiLightningBolt />
+      </span>
+      MyApp
+    </h1>
+  )
 }
