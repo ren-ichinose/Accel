8504f9a837de3d752c1f0ba2da09f55c9c33eb5b
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-20 16:12:50 +0900

    front_fix_framer-motion #62

--- frontend/src/components/common/layout/motion/motion.tsx
+++ frontend/src/components/common/layout/motion/motion.tsx
@@ -1,11 +1,14 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Motion({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname()
   return (
     <AnimatePresence mode="wait">
       <motion.div
+        key={pathName}
         initial={{ opacity: 0 }} // 初期状態
         animate={{ opacity: 1 }} // マウント時
         exit={{ opacity: 0 }} // アンマウント時
