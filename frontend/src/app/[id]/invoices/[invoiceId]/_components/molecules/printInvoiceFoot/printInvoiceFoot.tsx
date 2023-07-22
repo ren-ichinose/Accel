3af1672a35bcf313425c5a763cb901f5b85c1ed7
Ren Ichinose <kamatani.toru@gmail.com>
2023-07-22 09:55:39 +0900

    front_fix_mainFootWrapper #78

---
+++ frontend/src/app/[id]/invoices/[invoiceId]/_components/molecules/printInvoiceFoot/printInvoiceFoot.tsx
@@ -0,0 +1,10 @@
+import MainFootWrapper from '@/components/common/layout/mainFootWrapper/mainFootWrapper'
+import PrintInvoiceButton from '../../atoms/printInvoiceButton/printInvoice'
+
+export default function PrintInvoiceFoot() {
+  return (
+    <MainFootWrapper>
+      <PrintInvoiceButton />
+    </MainFootWrapper>
+  )
+}
--- frontend/src/app/[id]/invoices/[invoiceId]/page.tsx
+++ frontend/src/app/[id]/invoices/[invoiceId]/page.tsx
@@ -1,14 +1,14 @@
 import MainContentWrapper from '@/components/common/layout/mainContentWrapper/mainContetWrapper'
 import Motion from '@/components/common/layout/motion/motion'
 import MainHead from '@/components/mainHead/organisms/mainHead/mainHead'
-import PrintInvoiceButton from './_components/atoms/printInvoiceButton/printInvoice'
+import PrintInvoiceFoot from './_components/molecules/printInvoiceFoot/printInvoiceFoot'
 
 export default function ShowInvoice({ params }: { params: { id: string } }) {
   return (
     <Motion>
       <MainContentWrapper marginBottom="96px">
         <MainHead title="請求書の詳細" businessId={params.id} />
-        <PrintInvoiceButton />
+        <PrintInvoiceFoot />
       </MainContentWrapper>
     </Motion>
   )
